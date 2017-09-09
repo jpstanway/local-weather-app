a18b690763e3017e01dfd6c3ba9b65f0231beda3
Jordan Stanway <jpstanway@gmail.com>
2017-09-09 15:18:06 -0700

    Convert C to F and F to C when temp is clicked/pressed

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+var city = "";
+var country = "";
+var temp = 0;
+var hi_temp = 0;
+var lo_temp = 0;
+var scale = "";
+var condition = "";
+var icon = "";
 
 
 if (navigator.geolocation) {
@@ -15,9 +23,50 @@ function locOK(latitude, longitude) {
 
     console.log(data);
 
-    $('#location').html(data.name + ", " + data.sys.country);
-    $('.temp-display').append('<p>' + Math.floor(data.main.temp) + ' ° </p>'
-                              + '<img src=' + data.weather[0].icon + '>'
-                              + '<p><em>' + data.weather[0].main + '</em></p>');
+    city = data.name;
+    country = data.sys.country;
+    temp = Math.round(data.main.temp);
+    hi_temp = Math.round(data.main.temp_max);
+    lo_temp = Math.round(data.main.temp_min);
+    condition = data.weather[0].main;
+    icon = data.weather[0].icon;
+
+    if (country === "US") {
+      scale = "F";
+    } else {
+      scale = "C";
+    }
+
+    $('#location').html(city + ", " + country);
+    $('.conditions').html('<img src=' + icon + '>'
+                            + '<p><em>' + condition + '</em></p>');
+
+    setStats(temp, hi_temp, lo_temp, scale);
+
   });
 }
+
+function setStats(temp, hi_temp, lo_temp, scale) {
+
+  $('.display').html(temp + ' °' + scale);
+  $('.hi-lo').html('<p><strong>Hi:</strong> ' + hi_temp + ' °' + scale + '</p>'
+                      + '<p><strong>Lo:</strong> ' + lo_temp + ' °' + scale + '</p>');
+
+}
+
+$('#main-temp').on('click', function() {
+
+  if (scale === "C") {
+    temp = Math.round(temp * 1.8 + 32);
+    hi_temp = Math.round(hi_temp * 1.8 + 32);
+    lo_temp = Math.round(lo_temp * 1.8 + 32);
+    scale = "F";
+  } else if (scale === "F") {
+    temp = Math.round((temp - 32) / 1.8);
+    hi_temp = Math.round((hi_temp - 32) / 1.8);
+    lo_temp = Math.round((lo_temp - 32) / 1.8);
+    scale = "C";
+  }
+
+  setStats(temp, hi_temp, lo_temp, scale);
+});
